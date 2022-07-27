634df333bbcbcc3528b98d7187b04414da4f68fb
logzinga <65839350+logzinga@users.noreply.github.com>
2022-07-27 10:19:34 +1000

    Collision Reset

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter ()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Object Hit!");
     }
 }
