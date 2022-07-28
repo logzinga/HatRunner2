8cab428fbbc715cfc18cb35f25ab1318282fc4d3
logzinga <65839350+logzinga@users.noreply.github.com>
2022-07-28 21:12:44 +1000

    Update PlayerCollision.cs
    - Added some code notes

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-        public PlayerMovement playerMovement;
+        public PlayerMovement playerMovement; // Linking Player Movement script with Collision Script
 
     void OnCollisionEnter (Collision collisionInfo) // This so for when the Player Collides with an Object
     {
 
-        if (collisionInfo.collider.tag == "Obstacle")
+        if (collisionInfo.collider.tag == "Obstacle") // Does the actions when the Collider has the tag "Obstacle"
         {
-            Debug.Log("Obstacle Hit!");
-            playerMovement.enabled = false;
+            Debug.Log("Obstacle Hit!"); // Sends to Console that the Obstacle has been hit.
+            playerMovement.enabled = false; // Disables Player Movement
         }
     }
 }
