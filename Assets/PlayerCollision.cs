757500d0386888fa00f984e84562dbe8c836ab48
logzinga <65839350+logzinga@users.noreply.github.com>
2022-07-28 10:56:34 +1000

    Update PlayerCollision.cs

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -4,6 +4,6 @@ public class PlayerCollision : MonoBehaviour
 {
     void OnCollisionEnter (Collision collisionInfo) // This so for when the Player Collides with an Object, FIXME
     {
-        Debug.Log("Object Hit!" + collisionInfo); 
+        Debug.Log("Object Hit! " + collisionInfo); 
     }
 }
