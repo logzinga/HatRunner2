b5a30a0af5780f469354d007cf9e76feb224d1ec
log :3 <65839350+logzinga@users.noreply.github.com>
2022-09-05 23:05:36 +1000

    credit stuff
    - added animations
    
    - ultimately decided to load new scenes for the credits as it is easier to do.

---
+++ Assets/CreditsNext.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsNext : MonoBehaviour
+{
+    public void CreditsContinue ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // This loads the next level in the queue
+    }
+}
